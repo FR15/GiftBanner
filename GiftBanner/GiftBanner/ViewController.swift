23ef8f82375343e31b5512e8e98f10c3f3712316
Frank_s <frank_s@Frank-sdeMacBook-Pro.local>
2018-11-02 09:59:56 +0800

    init

---
+++ GiftBanner/GiftBanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiftBanner
+//
+//  Created by Frank_s on 2018/11/2.
+//  Copyright © 2018 Frank_s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
